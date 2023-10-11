d4ecb66d2487f9f41a04354306c4a1476b647932
UsmanSaeed787 <usmanalisaeed787@gmail.com>
2023-10-11 22:17:36 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+"use strict";
+// Comparison Operators
+var num1 = 10;
+var num2 = 20;
+// Comparison operator returns BOOLEAN type value.
+// less than operator
+var result = num1 < num2;
+console.log("Result_1 = ", result);
+// greater than operator
+var result2 = num1 > num2;
+console.log("Result_2 = ", result2);
+// equal to operator 
+//only value check.
+var result3 = num1 == num2;
+console.log("Result_3 = ", result3);
+// value check and datatype check.
+var result4 = num1 === num2;
+console.log("Result_4 = ", result4);
+//not equal to operator
+var result5 = num1 != num2;
+console.log("Result_5 = ", result5);
+//Greater than and equal to operator
+var result6 = num1 >= num2;
+console.log("Result_6 = ", result6);
+//Less than and equal to operator
+var result7 = num1 <= num2;
+console.log("Result_7 = ", result7);
---
+++ index.ts
@@ -0,0 +1,33 @@
+// Comparison Operators
+var num1: number = 10;
+var num2: number = 20;
+
+// Comparison operator returns BOOLEAN type value.
+
+// less than operator
+var result: boolean = num1 < num2;
+console.log("Result_1 = ",result);
+
+// greater than operator
+var result2: boolean = num1 > num2;
+console.log("Result_2 = ",result2);
+
+// equal to operator 
+//only value check.
+var result3: boolean = num1 == num2;
+console.log("Result_3 = ",result3);
+// value check and datatype check.
+var result4: boolean = num1 === num2;
+console.log("Result_4 = ",result4);
+
+//not equal to operator
+var result5: boolean = num1 != num2;
+console.log("Result_5 = ",result5);
+
+//Greater than and equal to operator
+var result6: boolean = num1 >= num2;
+console.log("Result_6 = ",result6);
+
+//Less than and equal to operator
+var result7: boolean = num1 <= num2;
+console.log("Result_7 = ",result7);
